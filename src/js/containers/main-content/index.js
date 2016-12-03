0779b816c75a474b6e37bedf59ae5b4c9bf65448
Victor Miguez <victorcineis@gmail.com>
2016-12-03 19:35:37 -0200

    Remove disclaimer

--- src/js/containers/main-content/index.js
+++ src/js/containers/main-content/index.js
@@ -3,13 +3,11 @@
 import React from 'react'
 import Header from './header'
 import Page from './page'
-import Aside from './aside'
 
 const MainContent = () => (
   <main className='main' role='main'>
     <Header />
     <Page />
-    <Aside />
   </main>
 
 )
