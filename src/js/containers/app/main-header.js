d69d77f06db86d0841d4bc7d1906ea0d0133636f
Guilherme Luchesi <gui.olhenrique@gmail.com>
2017-01-12 20:40:56 -0200

    Insere menu em um nav de role menu

--- src/js/containers/app/main-header.js
+++ src/js/containers/app/main-header.js
@@ -16,40 +16,53 @@ const MainHeader = () => (
     <a className='logo' href='./' role='logo'>
       <img src='img/logo.png' />
     </a>
-    <a
-      className='btn-link'
-      target='_blank'
-      href='https://frontendmeetup.typeform.com/to/vNNQyd'
-      title='Quero apresentar'>
-        Quero palestrar
-    </a>
-    <a
-      className='btn-link'
-      target='_blank'
-      href='https://frontendmeetup.typeform.com/to/XuXAD5'
-      title='Quero sediar'>
-        Quero sediar
-    </a>
-    <a
-      className='btn-link'
-      href='#manifest'
-      title='Manifesto Campinas front-end'>
-        Manifesto
-    </a>
-    <a
-      className='btn-link'
-      href='#conduct'
-      title='Código de conduta'>
-        Código de conduta
-    </a>
-
-    <a
-      className='btn-link'
-      target='_blank'
-      href='http://frontendcampinas.herokuapp.com/'
-      title='Slack'>
-        Slack
-    </a>
+    <nav className='menu' role='navigation'>
+      <ul>
+        <li>
+          <a
+            className='btn-link'
+            target='_blank'
+            href='https://frontendmeetup.typeform.com/to/vNNQyd'
+            title='Quero apresentar'>
+            Quero palestrar
+          </a>
+        </li>
+        <li>
+          <a
+            className='btn-link'
+            target='_blank'
+            href='https://frontendmeetup.typeform.com/to/XuXAD5'
+            title='Quero sediar'>
+            Quero sediar
+          </a>
+        </li>
+        <li>
+          <a
+            className='btn-link'
+            href='#manifest'
+            title='Manifesto Campinas front-end'>
+            Manifesto
+          </a>
+        </li>
+        <li>
+          <a
+            className='btn-link'
+            href='#conduct'
+            title='Código de conduta'>
+            Código de conduta
+          </a>
+        </li>
+        <li>
+          <a
+            className='btn-link'
+            target='_blank'
+            href='http://frontendcampinas.herokuapp.com/'
+            title='Slack'>
+            Slack
+          </a>
+        </li>
+      </ul>
+    </nav>
   </header>
 )
 
