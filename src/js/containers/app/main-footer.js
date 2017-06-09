4c980cdf236fb4f8d6be28ea78662a3e582c308e
Victor Miguez <victorcineis@gmail.com>
2017-06-09 00:20:49 -0300

    Removes github ribbon

--- src/js/containers/app/main-footer.js
+++ src/js/containers/app/main-footer.js
@@ -9,6 +9,8 @@ const MainFooter = () => (
         <p>
           Forked from
           <a href='https://github.com/frontendbr/eventos' target='_blank'> Front-End Brasil</a>
+          <span> | </span>
+          <a href='https://github.com/campinas-front-end' target='_blank'>GitHub</a>
         </p>
       </div>
     </div>
